ed3510c5fb6125e8fbf858ecc987d02f48f805e5
Christopher Falck <cfalck@rapid-genomics.com>
2016-12-17 19:34:11 -0500

    Express generator configured to serve a basic index.html file.

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var consolidate = require('consolidate');
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('html', consolidate.swig);
+app.set('views', path.join(__dirname, 'public', 'views'));
+app.set('view engine', 'html');
+
+// Direct urls to route handler implementations. 
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
